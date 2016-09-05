19d7f7456035537987f04a495b654e16fefa3e39
Jon Jones <jondewijones@gmail.com>
2016-09-05 15:13:33 +0100

    Grab, format and submit values

--- src/main/web/js/app/feedback-form.js
+++ src/main/web/js/app/feedback-form.js
@@ -2,14 +2,42 @@ $(function() {
     $('.js-feedback-form').on("submit",function(e) {
         e.preventDefault(); // cancel the actual submit
 
-        var $this = $(this),
+        var $form = $(this),
             msg = 'Thank you for your feedback',
-            msgHTML = $('<p class="margin-top--0">' + msg + '</p>');
+            msgHTML = $('<p class="margin-top--0">' + msg + '</p>'),
+            data = [],
+            dataStr = '';
 
-        // insert success message and hide form
-        msgHTML.insertAfter($this);
-        $this.hide();
+        // collect values and add to data array
+        var found = $('input[name=found]:checked', '.js-feedback-form').val();
+        if(found){found = "found=" + found; data.push(found)}
 
+        var understood = found = $('input[name=understood]:checked', '.js-feedback-form').val();
+        if(understood){understood = "understood=" + understood; data.push(understood)}
+
+        var email = $('#email').val();
+        if(email){email = "email=" + email; data.push(email)}
+
+        var feedback = $('#feedback').val();
+        if(feedback){feedback = "feedback=" + feedback; data.push(feedback)}
+
+        // create data string
+        for (var i = 0; i < data.length; i++) {
+            // if value exists, add value to data string
+            if(data[i]) {dataStr += data[i];}
+            // if not last in array add ampersand
+            if(i < (data.length - 1)) {dataStr += '&';}
+        }
+
+        // send to server
+        $.ajax({
+            url: '/feedback',
+            type: "post",
+            data: dataStr
+        }).done(function() {
+            msgHTML.insertAfter($form);
+            $form.hide();
+        });
 
     });
 });
