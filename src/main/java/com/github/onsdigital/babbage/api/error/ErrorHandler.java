ce2c23d5cfcec575931caa28aed6f7d614bf24ea
BarathaAberathne <barathaaberathne@gmail.com>
2024-04-10 14:08:11 +0100

    comment added

--- src/main/java/com/github/onsdigital/babbage/api/error/ErrorHandler.java
+++ src/main/java/com/github/onsdigital/babbage/api/error/ErrorHandler.java
@@ -47,7 +47,7 @@ public class ErrorHandler implements ServerError {
             renderErrorPage(501, response);
         } else {
             Exception e = (Exception) t;
-            //When user is not logged into the florence, expected response is 401. but currently babbage receives 200, with the following error message
+            // When user is not logged in and we request authenticated resources from Zebedee, the expected response is 401. Zebedee currently gives 200, with the following error message:
             if (e.getMessage().contains("Access Token required but none provided.") || e.getMessage().contains("JWT verification failed as token is expired.")){
                 error().exception(t).log("authorization error");
                 renderErrorPage(401, response);
