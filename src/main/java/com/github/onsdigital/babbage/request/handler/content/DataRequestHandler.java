ab4bd0893f692cf20a4cc2fc3d7201972613c696
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2025-03-05 11:48:32 +0000

    Remove aggregated data pages

--- src/main/java/com/github/onsdigital/babbage/request/handler/content/DataRequestHandler.java
+++ src/main/java/com/github/onsdigital/babbage/request/handler/content/DataRequestHandler.java
@@ -4,28 +4,17 @@ import com.github.onsdigital.babbage.content.client.ContentClient;
 import com.github.onsdigital.babbage.content.client.ContentReadException;
 import com.github.onsdigital.babbage.content.client.ContentResponse;
 import com.github.onsdigital.babbage.request.handler.base.BaseRequestHandler;
-import com.github.onsdigital.babbage.request.handler.base.ListRequestHandler;
 import com.github.onsdigital.babbage.response.BabbageContentBasedStringResponse;
 import com.github.onsdigital.babbage.response.BabbageStringResponse;
 import com.github.onsdigital.babbage.response.base.BabbageResponse;
-import com.github.onsdigital.babbage.util.URIUtil;
 import com.github.onsdigital.babbage.configuration.deprecation.DeprecationItem;
 
 import org.apache.hc.core5.http.HttpStatus;
-import org.reflections.Reflections;
-import org.reflections.util.ConfigurationBuilder;
 import javax.servlet.http.HttpServletRequest;
 
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import static com.github.onsdigital.babbage.util.RequestUtil.getQueryParameters;
-import static com.github.onsdigital.logging.v2.event.SimpleEvent.error;
-import static com.github.onsdigital.logging.v2.event.SimpleEvent.info;
-
 import static com.github.onsdigital.babbage.configuration.ApplicationConfiguration.appConfig;
 
 /**
@@ -34,8 +23,6 @@ import static com.github.onsdigital.babbage.configuration.ApplicationConfigurati
 public class DataRequestHandler extends BaseRequestHandler {
 
     public static final String REQUEST_TYPE = "data";
-    private static Map<String, ListRequestHandler> listPageHandlers = new HashMap<>();
-
     private Map<String, DeprecationItem> deprecationItemMap;
 
     public DataRequestHandler() {
@@ -58,13 +45,6 @@ public class DataRequestHandler extends BaseRequestHandler {
     }
 
     public BabbageResponse getData(String uri, HttpServletRequest request) throws Exception {
-
-        String requestType = URIUtil.resolveRequestType(uri);
-
-        if (listPageHandlers.containsKey(requestType)) {
-            return listPageHandlers.get(requestType).getData(URIUtil.removeLastSegment(uri), request);
-        }
-
         ContentResponse contentResponse = getContent(uri, getQueryParameters(request));
         String pageType = contentResponse.getPageType();
 
@@ -85,37 +65,6 @@ public class DataRequestHandler extends BaseRequestHandler {
             }
         }
         return babbageResponse;
-    }
-
-    static {
-        registerListHandlers();
-    }
-
-    private synchronized static void registerListHandlers() {
-        try {
-            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
-                    .addUrls(BaseRequestHandler.class.getProtectionDomain().getCodeSource().getLocation());
-            configurationBuilder.addClassLoaders(BaseRequestHandler.class.getClassLoader());
-            Set<Class<? extends ListRequestHandler>> requestHandlerClasses = new Reflections(configurationBuilder)
-                    .getSubTypesOf(ListRequestHandler.class);
-
-            for (Class<? extends ListRequestHandler> handlerClass : requestHandlerClasses) {
-                if (!Modifier.isAbstract(handlerClass.getModifiers())) {
-                    String className = handlerClass.getSimpleName();
-                    ListRequestHandler handlerInstance = handlerClass.newInstance();
-                    listPageHandlers.put(handlerInstance.getRequestType(), handlerInstance);
-                }
-            }
-
-            info().data("handlers", Arrays.toString(listPageHandlers.entrySet()
-                    .stream()
-                    .map((e) -> e.getValue().getClass().getName()).toArray()))
-                    .log("registered list page request handlers");
-
-        } catch (Exception ex) {
-            error().exception(ex).log("error failed to initialize request handler");
-            throw new RuntimeException(ex);
-        }
     }
 
     /**
