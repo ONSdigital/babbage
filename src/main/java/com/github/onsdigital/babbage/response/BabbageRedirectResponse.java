21ed98ba4d43c7686c838e41d105b9ff3ee14806
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2025-06-19 12:11:06 +0100

    Fix redirects to relative path only

--- src/main/java/com/github/onsdigital/babbage/response/BabbageRedirectResponse.java
+++ src/main/java/com/github/onsdigital/babbage/response/BabbageRedirectResponse.java
@@ -1,7 +1,6 @@
 package com.github.onsdigital.babbage.response;
 
 import com.github.onsdigital.babbage.response.base.BabbageResponse;
-import org.eclipse.jetty.http.HttpHeader;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,30 +21,10 @@ public class BabbageRedirectResponse extends BabbageResponse {
 
     @Override
     public void apply(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String forwardedHost = request.getHeader(HttpHeader.X_FORWARDED_HOST.asString());
-        String forwardedProto = request.getHeader(HttpHeader.X_FORWARDED_PROTO.asString());
-
         response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-
-        if ((null != forwardedHost && !forwardedHost.isEmpty()) && (null != forwardedProto && !forwardedProto.isEmpty())) {
-            String url = buildHttpsRedirectUrl(forwardedProto, forwardedHost, redirectUri);
-            response.setHeader(HttpHeaders.LOCATION, url);
-        } else {
-            response.setHeader(HttpHeaders.LOCATION, redirectUri);
-        }
+        response.setHeader(HttpHeaders.LOCATION, redirectUri);
     }
 
-    /**
-     *
-     * @param scheme
-     * @param host
-     * @param redirectUri
-     * @return
-     */
-    private static String buildHttpsRedirectUrl(String scheme, String host, String redirectUri) {
-        redirectUri = !redirectUri.startsWith("/") ? String.format("/%s", redirectUri) : redirectUri;
-        return scheme + "://" + host + redirectUri;
-    }
 
     public String getRedirectUri() {
         return redirectUri;
